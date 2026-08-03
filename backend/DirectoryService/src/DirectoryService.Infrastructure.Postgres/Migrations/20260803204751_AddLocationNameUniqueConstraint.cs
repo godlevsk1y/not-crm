75e50648231331cf313ca7d5a7c6541306f8a1dd
godlevsk1y <godlevskiy.is.working@gmail.com>
2026-08-03 23:49:56 +0300

    fix: add unique constraint to the Location.Name

---
+++ backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/Migrations/20260803204751_AddLocationNameUniqueConstraint.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DirectoryService.Infrastructure.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocationNameUniqueConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "uq_locations_name",
+                table: "locations",
+                column: "name",
+                unique: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "uq_locations_name",
+                table: "locations"
+            );
+        }
+    }
+}
